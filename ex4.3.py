8a6091194dbd644bae12cbc446a6989bcd469bea
Domenico Cristo <84397096+domenicocristo@users.noreply.github.com>
2023-02-12 00:55:59 +0100

    Update ex4.3.py

--- ex4.3.py
+++ ex4.3.py
@@ -1,12 +1,15 @@
-# 1. Scrivete una funzione di nome quadrato che richieda un parametro di nome t, che è
-# una tartaruga. La funzione deve usare la tartaruga per disegnare un quadrato.
-# Scrivete una chiamata alla funzione quadrato che passi bob come argomento, ed
-# eseguite nuovamente il programma.
-
-# 2. Aggiungete a quadrato un nuovo parametro di nome lunghezza. Modificate il corpo 
-# in modo che la lunghezza dei lati sia pari a lunghezza, quindi modificate la chiamata 
-# alla funzione in modo da fornire un secondo argomento. Eseguite di nuovo il 
-# programma e provatelo con vari valori di lunghezza.
+"""
+1. Scrivete una funzione di nome quadrato che richieda un parametro di nome t, che è
+una tartaruga. La funzione deve usare la tartaruga per disegnare un quadrato.
+Scrivete una chiamata alla funzione quadrato che passi bob come argomento, ed
+eseguite nuovamente il programma.
+"""
+"""
+2. Aggiungete a quadrato un nuovo parametro di nome lunghezza. Modificate il corpo 
+in modo che la lunghezza dei lati sia pari a lunghezza, quindi modificate la chiamata 
+alla funzione in modo da fornire un secondo argomento. Eseguite di nuovo il 
+programma e provatelo con vari valori di lunghezza.
+"""
 
 import turtle, math
 bob = turtle.Turtle()
@@ -18,9 +21,11 @@ def quadrato(t, lunghezza):
 
 quadrato(bob, lunghezza=100)
 
-# 3. Fate una copia di quadrato e cambiate il nome in poligono. Aggiungete un altro
-# parametro di nome n e modificate il corpo in modo che sia disegnato un poligono 
-# regolare di n lati.
+"""
+3. Fate una copia di quadrato e cambiate il nome in poligono. Aggiungete un altro
+parametro di nome n e modificate il corpo in modo che sia disegnato un poligono 
+regolare di n lati.
+"""
 
 def poligono(t, n, lunghezza):
     angolo = 360 / n
@@ -30,9 +35,11 @@ def poligono(t, n, lunghezza):
 
 poligono(bob, n=7, lunghezza=100)
 
-# 4. Scrivete una funzione di nome cerchio che prenda come parametri una tartaruga, t,
-# e un raggio, r, e che disegni un cerchio approssimato chiamato poligono con una 
-# appropriata lunghezza e numero di lati. Provate la funzione con diversi valori di r.
+"""
+4. Scrivete una funzione di nome cerchio che prenda come parametri una tartaruga, t,
+e un raggio, r, e che disegni un cerchio approssimato chiamato poligono con una 
+appropriata lunghezza e numero di lati. Provate la funzione con diversi valori di r.
+"""
 
 def cerchio(t, r):
     circonferenza = 2 * math.pi * r
@@ -42,9 +49,11 @@ def cerchio(t, r):
 
 cerchio(bob, 100)
 
-# 5. Create una versione più generale della funzione del cerchio, di nome arco che richieda 
-# un parametro aggiuntivo angolo, il quale determina la porzione di cerchio da disegnare.
-# Angolo è espresso in gradi, quindi se angolo=360, arco deve disegnare un cerchio completo.
+"""
+5. Create una versione più generale della funzione del cerchio, di nome arco che richieda 
+un parametro aggiuntivo angolo, il quale determina la porzione di cerchio da disegnare.
+Angolo è espresso in gradi, quindi se angolo=360, arco deve disegnare un cerchio completo.
+"""
 
 def arco(t, r, angolo):
     arco_lunghezza = 2 * math.pi * r * angolo / 360
